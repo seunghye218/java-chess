3bec40ebf3c0336acedb7c7a9d4bd6944afce1ec
seunghye218 <seunghye@student.42seoul.kr>
2024-03-30 21:39:08 +0900

    feat: status 입력 기능 구현

--- src/main/java/chess/view/InputView.java
+++ src/main/java/chess/view/InputView.java
@@ -8,6 +8,7 @@ public class InputView {
     private static final String COMMAND_START = "start";
     private static final String COMMAND_MOVE = "move";
     private static final String COMMAND_END = "end";
+    private static final String COMMAND_STATUS = "status";
 
     public void readStart() {
         if (!COMMAND_START.equals(SCANNER.nextLine())) {
@@ -17,9 +18,9 @@ public class InputView {
 
     public String readCommand() {
         String input = SCANNER.nextLine();
-        if (!COMMAND_END.equals(input) && !input.startsWith(COMMAND_MOVE)) {
+        if (!COMMAND_END.equals(input) && !COMMAND_STATUS.equals(input) && !input.startsWith(COMMAND_MOVE)) {
             throw new IllegalArgumentException(
-                    String.format("게임 중에는 %s, %s 명령만 입력할 수 있습니다.", COMMAND_MOVE, COMMAND_MOVE));
+                    String.format("게임 중에는 %s, %s, %s 명령만 입력할 수 있습니다.", COMMAND_MOVE, COMMAND_END, COMMAND_STATUS));
         }
         return input;
     }
