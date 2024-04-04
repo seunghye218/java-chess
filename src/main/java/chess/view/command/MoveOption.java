ed3fd5a1cfbad59e732a1d8e57bce72d442bcf7f
seunghye218 <seunghye@student.42seoul.kr>
2024-04-05 02:06:04 +0900

    fix: move 명령의 인자가 2개가 아닐 경우 버그

--- src/main/java/chess/view/command/MoveOption.java
+++ src/main/java/chess/view/command/MoveOption.java
@@ -2,18 +2,31 @@ package chess.view.command;
 
 import java.util.List;
 
-public record MoveOption(String source, String target) {
+public class MoveOption {
 
     private static final int SOURCE_INDEX = 0;
     private static final int TARGET_INDEX = 1;
 
-    public MoveOption {
-        if (source.length() != 2 || target.length() != 2) {
-            throw new IllegalArgumentException("각 위치는 두글자 입니다. ex) 'a1', 'h8'");
-        }
-    }
+    private final String source;
+    private final String target;
+
 
     public MoveOption(List<String> options) {
-        this(options.get(SOURCE_INDEX), options.get(TARGET_INDEX));
+        if (options.size() != 2) {
+            throw new IllegalArgumentException("move 명령은 두 개의 위치를 입력해야 합니다.");
+        }
+        if (options.get(SOURCE_INDEX).length() != 2 || options.get(TARGET_INDEX).length() != 2) {
+            throw new IllegalArgumentException("각 위치는 두글자 입니다. ex) 'a1', 'h8'");
+        }
+        this.source = options.get(SOURCE_INDEX);
+        this.target = options.get(TARGET_INDEX);
+    }
+
+    public String source() {
+        return source;
+    }
+
+    public String target() {
+        return target;
     }
 }
--- src/test/java/chess/view/command/MoveOptionTest.java
+++ src/test/java/chess/view/command/MoveOptionTest.java
@@ -2,6 +2,7 @@ package chess.view.command;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,7 +11,7 @@ class MoveOptionTest {
     @DisplayName("MoveOption 인자가 2글자가 아니라면 예외를 던진다.")
     @Test
     void invalidCreate() {
-        assertThatThrownBy(() -> new MoveOption("a12", "b123"))
+        assertThatThrownBy(() -> new MoveOption(List.of("a12", "b123")))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
